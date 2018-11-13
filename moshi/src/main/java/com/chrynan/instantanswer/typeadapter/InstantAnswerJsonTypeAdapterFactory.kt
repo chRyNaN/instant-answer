6df9b58cd0c407a4635f622a4975f7b71570ccf9
Chris <ckeenan@starry.com>
2018-11-12 19:48:30 -0500

    Updated and Fixed InstantAnswerJsonTypeAdapterFactory

--- moshi/src/main/java/com/chrynan/instantanswer/typeadapter/InstantAnswerJsonTypeAdapterFactory.kt
+++ moshi/src/main/java/com/chrynan/instantanswer/typeadapter/InstantAnswerJsonTypeAdapterFactory.kt
@@ -2,9 +2,6 @@
 
 package com.chrynan.instantanswer.typeadapter
 
-import com.chrynan.instantanswer.InstantAnswerJson
-import com.chrynan.instantanswer.MetaJson
-import com.chrynan.instantanswer.WebIconJson
 import com.chrynan.instantanswer.api.*
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
@@ -12,17 +9,28 @@ import java.lang.reflect.Type
 
 class InstantAnswerJsonTypeAdapterFactory : JsonAdapter.Factory {
 
-    override fun create(type: Type, annotations: MutableSet<out Annotation>, moshi: Moshi): JsonAdapter<*>? =
-        when (type) {
-            is InstantAnswer -> moshi.adapter(InstantAnswerJson::class.java)
-            is Result -> ResultTypeAdapter(moshi)
-            is Meta -> moshi.adapter(MetaJson::class.java)
-            is SourceOptions -> moshi.adapter(MetaJson.SourceOptionsJson::class.java)
-            is Developer -> moshi.adapter(MetaJson.DeveloperJson::class.java)
-            is Maintainer -> moshi.adapter(MetaJson.MaintainerJson::class.java)
-            is WebIcon -> moshi.adapter(WebIconJson::class.java)
-            else -> null
+    private val typeAdapters: MutableMap<String, JsonAdapter<*>> = mutableMapOf()
+
+    override fun create(type: Type, annotations: MutableSet<out Annotation>, moshi: Moshi): JsonAdapter<*>? {
+        var adapter = typeAdapters[type.typeName]
+
+        if (adapter == null && type is Class<*> && type.isSupportedType()) {
+            adapter = moshi.adapter(type)
+
+            typeAdapters[type.typeName] = adapter
         }
+
+        return adapter
+    }
+
+    private fun Class<*>.isSupportedType() =
+        (this == InstantAnswer::class.java) ||
+                (this == Result::class.java) ||
+                (this == Meta::class.java) ||
+                (this == SourceOptions::class.java) ||
+                (this == Developer::class.java) ||
+                (this == Maintainer::class.java) ||
+                (this == WebIcon::class.java)
 }
 
 fun Moshi.Builder.addInstantAnswerTypeFactory(): Moshi.Builder = add(InstantAnswerJsonTypeAdapterFactory())
